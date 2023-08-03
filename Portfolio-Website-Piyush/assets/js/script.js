37b24e76ef39c4d1fcdc376ea1959637e2810494
Piyush Sharma <111640586+Piyushsharma2004@users.noreply.github.com>
2023-08-03 13:12:08 +0530

    Update script.js

--- Portfolio-Website-Piyush/assets/js/script.js
+++ Portfolio-Website-Piyush/assets/js/script.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Piyush Sharma";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
