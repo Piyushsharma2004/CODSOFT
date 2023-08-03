4b388932fbd9cf6be7efc5b94f324bc56a4c3582
Piyush Sharma <111640586+Piyushsharma2004@users.noreply.github.com>
2023-08-03 13:11:21 +0530

    Update script.js

--- Portfolio-Website-Piyush/assets/js/script.js
+++ Portfolio-Website-Piyush/assets/js/script.js
@@ -192,15 +192,20 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64cb5988cc26a871b02cf863/1h6t5bfg0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
 })();
+</script>
+<!--End of Tawk.to Script-->
+
 // End of Tawk.to Live Chat
 
 
